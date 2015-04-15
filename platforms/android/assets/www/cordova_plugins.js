83d13e7d7c9c485265d07d85828d3a8d59d91ea9
syazani najamin <syazaninajamin@yahoo.com>
2015-04-15 11:53:17 +0800

    workshop

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device": "0.3.0",
+    "org.apache.cordova.console": "0.2.13",
+    "org.apache.cordova.inappbrowser": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
